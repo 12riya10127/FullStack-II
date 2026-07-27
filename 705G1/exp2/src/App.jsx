afcb25b71f5f2da091f5479973f6408f9705ec1a
suhaibkhan <johnxuhaib@gmail.com>
2026-07-27 13:14:09 +0530

    27july

--- 705G1/exp2/src/App.jsx
+++ 705G1/exp2/src/App.jsx
@@ -1,26 +1,54 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
   const [data, setData] = useState([]);
+  const [error, setError] = useState(false)
+  const [message, setMessage] = useState("")
+  const [loading, setLoading] = useState(false)
+  
+  
   useEffect(() => {
     async function fetchUsers() {
-      const response = await fetch('https://jsonplaceholder.typicode.com/users')
-      console.log(response)
-    setData(await response.json())
-      console.log(data)
+
+      try {
+        setLoading(true)
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+        if (!response.ok) {
+          throw new Error("Failed to Fetch Data ...")
+        }
+        console.log(response)
+        const data = await response.json()
+        console.log(data)
+        setData(data)
+        setLoading(false)
+        
+      } catch (error) { 
+        console.log(error.message)
+        setError(true)
+        setMessage(error.message)
+        setLoading(false)
+
+      }
+
+      
     }
     fetchUsers();
   }, [])
   return (
-    <div>App
-    
-      {data.map((usr) => (
-        <div>
+    <div>
+      {error && <h1>{message}</h1>}
+      {loading&&<h1>Loading ...</h1>}
+
+      {data.map((usr,index) => (
+        <div key={index}>
           <h3>{usr.name} </h3>
           <p>{usr.email}</p>
-          </div>   
+          <p>{usr.address.street}</p>
+          <p>{ usr.address.geo.lat}</p>
+        </div>
 
-  ))}
+      ))}
     </div>
   )
 }
