4005d3d7c60557f48b9feea7bdd8a40d4e150a27
suhaibkhan <johnxuhaib@gmail.com>
2026-08-07 14:24:01 +0530

    703g2

--- 703G2/exp3/src/pages/StudentList.jsx
+++ 703G2/exp3/src/pages/StudentList.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router'
-
+import students from '../data/students'
 function StudentList() {
   return (
     <div>
-      StudentList
-      <Link to="/dashboard/profile" >View Profile</Link>
+      <h4>Student List</h4>
+      
+      {students.map((std,index) => (
+        <div>
+          <h6>{std.name}
+            <Link to={`/dashboard/profile/${std.id}`} >View Profile</Link>
+          </h6>
+       
+        </div>
+
+      ))}
+
+
+
+
+   
       <Outlet/>
     
     </div>
--- 703G2/exp3/src/pages/StudentProfile.jsx
+++ 703G2/exp3/src/pages/StudentProfile.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useParams } from "react-router";
 
 function StudentProfile() {
+  const { id } = useParams()
+  console.log(id)
   return (
     <div>StudentProfile</div>
   )
